a28914ca6db21f9d8de34261d6b9431e53d2cdea
Tyler98ky <tyler98ky@gmail.com>
2018-10-16 23:21:13 -0400

    Added spanish translations

--- app/src/main/java/edu/temple/coloractivity/CustomAdapter.java
+++ app/src/main/java/edu/temple/coloractivity/CustomAdapter.java
@@ -1,7 +1,9 @@
 package edu.temple.coloractivity;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Color;
+import android.print.PrintAttributes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
@@ -24,8 +26,11 @@ public class CustomAdapter extends ArrayAdapter<String> {
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         String color = mColors[position];
 
+        Resources res = getContext().getResources();
+        String[] colors = res.getStringArray(R.array.string_array_name);
+
         TextView tv = (TextView) super.getView(position, convertView, parent);
-        tv.setText(color);
+        tv.setText(colors[position]);
         tv.setBackgroundColor(Color.parseColor(color));
 
         return tv;
@@ -35,7 +40,11 @@ public class CustomAdapter extends ArrayAdapter<String> {
     public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         String color = mColors[position];
 
+        Resources res = getContext().getResources();
+        String[] colors = res.getStringArray(R.array.string_array_name);
+
         TextView tv = (TextView) super.getDropDownView(position, convertView, parent);
+        tv.setText(colors[position]);
         tv.setBackgroundColor(Color.parseColor(color));
 
         return tv;
